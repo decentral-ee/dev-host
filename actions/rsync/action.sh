5c068e0203c2e15aeddf8539c56857f8d45746b1
Miao ZhiCheng <miao@decentral.ee>
2021-05-02 15:27:57 +0300

    update action

--- actions/rsync/action.sh
+++ actions/rsync/action.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 # create temporary ssh key
-echo "$INPUT_DEV_HOST_SSH_KEY" > ssh.key
+echo "${INPUT_DEV_HOST_SSH_KEY}" > ssh.key
 chmod 600 ssh.key
 
 set -ex
-
-export
 
 SSH_OPTS="-o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 
 rsync -Pav \
   -e "ssh -i ssh.key $SSH_OPTS" \
-  $INPUT_LOCAL_PATH \
-  $INPUT_DEV_HOST_USERNAME@$INPUT_DEV_HOST_HOSTNAME:$INPUT_REMOTE_PATH
+  ${INPUT_LOCAL_PATH} \
+  ${INPUT_DEV_HOST_USERNAME}@${INPUT_DEV_HOST_HOSTNAME}:${INPUT_REMOTE_PATH}
