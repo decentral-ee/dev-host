33ef2dea76796fd49440720864aeddd297ae48fe
Miao ZhiCheng <miao@decentral.ee>
2021-05-07 20:03:59 +0300

    fixes

--- actions/rsync/action.sh
+++ actions/rsync/action.sh
@@ -2,9 +2,17 @@
 
 source actions/prepare_ssh_action.sh
 
+REMOTE_PATH=${INPUT_DEV_HOST_USER}@${INPUT_DEV_HOST_HOSTNAME}:${INPUT_REMOTE_PATH}
 # We do not use "--delete", because some error in workflow could lead
 # to a wipeout of data.
-rsync -Pav \
-  -e "ssh -i ssh.key ${SSH_OPTS}" \
-  ${INPUT_LOCAL_PATH} \
-  ${INPUT_DEV_HOST_USER}@${INPUT_DEV_HOST_HOSTNAME}:${INPUT_REMOTE_PATH}
+if [ -z "$INPUT_DOWNLOAD" ];then
+    rsync -Pav \
+      -e "ssh -i ssh.key ${SSH_OPTS}" \
+      ${INPUT_LOCAL_PATH} \
+      ${REMOTE_PATH}
+else
+    rsync -Pav \
+      -e "ssh -i ssh.key ${SSH_OPTS}" \
+      ${REMOTE_PATH} \
+      ${INPUT_LOCAL_PATH} \
+fi
