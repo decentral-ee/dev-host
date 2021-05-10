eac18f1f4d08c3edd585643f33b1a9e8713a8776
Miao ZhiCheng <miao@decentral.ee>
2021-05-10 23:47:59 +0300

    imporve run-script action

--- actions/run-script/action.sh
+++ actions/run-script/action.sh
@@ -4,8 +4,15 @@ export INPUT_LOCAL_PATH=$INPUT_SCRIPT_PATH
 export INPUT_REMOTE_PATH=/tmp/action-$RANDOM-$(basename "$INPUT_SCRIPT_PATH")
 "$(dirname "$0")"/../rsync/action.sh
 
-export INPUT_COMMAND="$INPUT_SCRIPT_SHELL $INPUT_REMOTE_PATH $INPUT_SCRIPT_ARGS"
-"$(dirname "$0")"/../exec/action.sh
+# cleanup the remote script always
 
-export INPUT_COMMAND="rm -f $INPUT_REMOTE_PATH"
+function finish {
+    export INPUT_COMMAND="rm -f $INPUT_REMOTE_PATH"
+    "$(dirname "$0")"/../exec/action.sh
+}
+trap finish EXIT
+
+# fail the script if this exec action fail
+set -e
+export INPUT_COMMAND="$INPUT_SCRIPT_SHELL $INPUT_REMOTE_PATH $INPUT_SCRIPT_ARGS"
 "$(dirname "$0")"/../exec/action.sh
