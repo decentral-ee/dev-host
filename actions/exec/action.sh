fd5fc7bbb9bf15cb0f1cdc6f7d2523f29de6854e
Miao ZhiCheng <hellwolf.misty@gmail.com>
2021-05-10 19:24:35 +0300

    Feature action run script (#2)

--- actions/exec/action.sh
+++ actions/exec/action.sh
@@ -4,4 +4,4 @@ source "$(dirname "$0")"/../prepare_ssh_action.sh
 
 ssh -i ssh.key ${SSH_OPTS} \
     ${INPUT_DEV_HOST_USER}@${INPUT_DEV_HOST_HOSTNAME} -- \
-    ${INPUT_COMMAND}
+    "${INPUT_COMMAND}"
